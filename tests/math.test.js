f08cc60948ae2138b62c579ccc60dbef90be7b18
Bruno Amaral <70671901+Brn-a@users.noreply.github.com>
2026-08-18 01:01:42 +0000

    correção da função

--- tests/math.test.js
+++ tests/math.test.js
@@ -5,7 +5,7 @@ test('soma 2 + 3 igual a 5', () => {
 
 });
 
-test('subitração 2 - 3 igual a -5', () => {
-    expect(math.subtract(2, 3)).toBe(-5);
+test('subitração 2 - 3 igual a -1', () => {
+    expect(math.subtract(2, 3)).toBe(-1);
 
 });
